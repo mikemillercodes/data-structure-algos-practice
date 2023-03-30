c7fafe1d1bf21371f4c9e7ca060ad2492ebebc6b
Mike Miller <millermichaelg1@gmail.com>
2023-03-30 14:08:08 -0500

    BFS values on Structy

--- 3.30.23_pairboarding.js
+++ 3.30.23_pairboarding.js
@@ -19,3 +19,21 @@ const depthFirstValues = (root) => {
     
     return result;
   };
+
+  const breadthFirstValues = (root) => {
+    if (!root) return [];
+    
+    const values = [];
+    
+    const queue = [ root ]
+    
+    while (queue.length) {
+      let node = queue.shift()
+      values.push(node.val)
+      
+      if (node.left) queue.push(node.left)
+      if (node.right) queue.push(node.right)
+      
+    }
+    return values
+  };
