69b5d804d2584629af4fe1a4e1aab09e341e627e
Mike Miller <millermichaelg1@gmail.com>
2023-04-04 16:36:39 -0500

    treeSum_structy

---
+++ treesum.js
@@ -0,0 +1,21 @@
+const treeSum = (root) => {
+    // todo
+    if (!root) return 0;
+    
+    let sum = 0; 
+    const stack = [ root ]; // [ ]
+    
+    while (stack.length) {
+      const current = stack.pop(); // 1
+      sum += current.val; // 21
+      
+      if (current.right !== null) {
+        stack.push(current.right); 
+      }
+      if (current.left !== null) {
+        stack.push(current.left);
+      }
+    }
+    return sum;
+    
+  };
